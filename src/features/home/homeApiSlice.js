78a766a6b229d7068b6912b2088a5fa0782d8b12
Admin <shivendrawk@gmail.com>
2025-04-23 13:09:53 +0530

    Fix server issue

--- src/features/home/homeApiSlice.js
+++ src/features/home/homeApiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const homeApiSlice = createApi({
     reducerPath: "homeFirestoreApi",
-    baseQuery: fetchBaseQuery({baseUrl: "http://localhost:5000/api"}),
+    baseQuery: fetchBaseQuery({baseUrl: "https://swiggy-clone-klzu.onrender.com/api"}),
     keepUnusedDataFor: 60,
 
     endpoints : builder => ({
