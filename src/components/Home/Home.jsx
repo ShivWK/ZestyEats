350c6769c751fcd07cc1ab5c32d21ab7a23a78fe
Admin <shivendrawk@gmail.com>
2025-05-04 19:23:05 +0530

    Fix code

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -17,7 +17,62 @@ import {
 export default function Home() {
   const topRestaurantsChainsData = useSelector(selectTopRestaurantsData);
   const FoodieThoughtsData = useSelector(selectFoodieThoughtsData);
+  // const [
+  //     triggerLoactionByCoordinates,
+  //     { isLoading: LocationByCoordinatesLoading },
+  //   ] = useLazyLocationByCoordinatesQuery();
+  // const [triggerRestaurentDataCall] = useLazyGetHomePageDataQuery();
 
+  // useEffect(() => {
+  //   if (navigator.geolocation) {
+  //         navigator.geolocation.getCurrentPosition(async (position) => {
+  //           const lat1 = position.coords.latitude;
+  //           const lng1 = position.coords.longitude;
+    
+  //           try {
+  //             const data = await triggerLoactionByCoordinates({
+  //               lat1,
+  //               lng1,
+  //             }).unwrap();
+
+  //             const localityObject = data?.data?.[0]?.["address_components"].find(
+  //               (item) => item?.["types"].includes("locality")
+  //             );
+  //             // Loader required
+  //             dispatch(addYourCurrentCity(localityObject?.["short_name"]));
+  //             dispatch(
+  //               addSearchedCityAddress(data?.data?.[0]?.["formatted_address"])
+  //             );
+    
+  //             const lat = data?.data?.[0]?.geometry?.location?.lat;
+  //             const lng = data?.data?.[0]?.geometry?.location?.lng;
+    
+  //             try {
+  //               const res2 = await triggerRestaurentDataCall({ lat, lng });
+  //               console.log(res2);
+  //               updateHomeRestaurantData(res2); //loader rquired
+  //               dispatch(addTopRestaurantsTitle(res2));
+  //             } catch (err) {
+  //               alert(err.message);
+  //             }
+  //           } catch (err) {
+  //             console.log("Error fetching current location data.");
+  //           }
+  //         });
+  //       } else {
+  //         // const lat: 26.8496217,
+  //         // const lng: 81.0072193,
+  //         // try {
+  //         //   const res2 = await triggerRestaurentDataCall({ lat, lng });
+  //         //   console.log(res2);
+  //         //   updateHomeRestaurantData(res2); //loader rquired
+  //         //   dispatch(addTopRestaurantsTitle(res2));
+  //         // } catch (err) {
+  //         //   alert(err.message);
+  //         // }
+  //       }
+  // }, [])
+  
   const dispatch = useDispatch();
   const { data, isLoading } = useGetHomePageDataQuery({
     lat: 26.8496217,
