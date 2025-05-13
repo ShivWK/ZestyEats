f6efd9d44eb713cab102b83f465e87abb3fd17a7
Admin <shivendrawk@gmail.com>
2025-05-13 17:48:08 +0530

    Fix footer

--- src/components/Footer/PageFooter.jsx
+++ src/components/Footer/PageFooter.jsx
@@ -7,8 +7,8 @@ export default function PageFooter() {
   return (
     !isLoading && (
       <footer className="h-20 w-full flex justify-center items-start bg-[#ff5200]">
-        <div className="border-2 flex max-w-[1210px] items-center text-white font-bold h-16 w-full mt-3">
-          d
+        <div className=" flex max-w-[1210px] items-center text-white font-bold h-16 w-full mt-3">
+          
         </div>
       </footer>
     )
