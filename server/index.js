1dc87df92847e2f2bd0be2ea37caf0235f5a2766
Admin <shivendrawk@gmail.com>
2025-06-04 13:26:26 +0530

    Fix server Problem

--- server/index.js
+++ server/index.js
@@ -1,6 +1,23 @@
 const express = require("express");
 const axios = require("axios");
 const cors = require("cors");
+const tough = require("tough-cookie");
+const axiosCookieJarSupport = require("axios-cookiejar-support").default;
+
+const jar = new tough.CookieJar();
+const client = axiosCookieJarSupport.wrapper(
+  axios.create({
+    jar,
+    headers: {
+      "User-Agent": "Mozilla/5.0",
+      Accept: "application/json",
+      "Accept-Language": "en-US,en;q=0.9",
+      Referer: "https://www.swiggy.com/",
+      Origin: "https://www.swiggy.com",
+    },
+    withCredentials: true,
+  })
+);
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -19,11 +36,8 @@ app.use(
         callback(new Error("Not allowed by CORS"));
       }
     },
-    credentials: true, 
   })
 );
-
-
 app.use(express.json());
 
 app.get("/", (req, res) => {
@@ -39,7 +53,6 @@ app.get("/", (req, res) => {
 });
 
 app.get("/api/swiggy-restaurants", async (req, res) => {
-
   console.log("by Browser", req.headers);
 
   try {
@@ -53,27 +66,12 @@ app.get("/api/swiggy-restaurants", async (req, res) => {
 
     const swiggyURL = `https://www.swiggy.com/dapi/restaurants/list/v5?lat=${lat}&lng=${lng}&page_type=DESKTOP_WEB_LISTING`;
 
-    const response = await axios.get(swiggyURL, {
-      headers: {
-        "User-Agent": "Mozilla/5.0",
-        Accept: "application/json",
-        "Accept-Language": "en-US,en;q=0.9",
-        Referer: "https://www.swiggy.com/",
-        Origin: "https://www.swiggy.com",
-
-      },
-
-      // withCredentials: true,
-    });
+    const response = await client.get(swiggyURL);
     // console.log(response.headers);
     const origin = req.headers.origin;
-    const cookies = response.headers["set-cookie"];
-    if (cookies) {
-      res.setHeader("set-cookie", cookies);
-    }
     res.set({
       "Access-Control-Allow-Origin": origin,
-      "Access-Control-Allow-Methods": "GET"
+      "Access-Control-Allow-Methods": "GET",
     });
     res.status(200).json(response.data);
   } catch (error) {
@@ -94,7 +92,7 @@ app.get("/api/place-autocomplete", async (req, res) => {
 
     const swiggyURL = `https://www.swiggy.com/dapi/misc/place-autocomplete?input=${input}&types=`;
 
-    const response = await axios.get(swiggyURL, {
+    const response = await client.get(swiggyURL, {
       headers: {
         "User-Agent": "Mozilla/5.0",
         Accept: "application/json",
@@ -103,7 +101,12 @@ app.get("/api/place-autocomplete", async (req, res) => {
         Origin: "https://www.swiggy.com",
       },
     });
-    res.setHeader("Access-Control-Allow-Origin", "*");
+
+    const origin = req.headers.origin;
+    res.set({
+      "Access-Control-Allow-Origin": origin,
+      "Access-Control-Allow-Methods": "GET",
+    });
     res.status(200).json(response.data);
   } catch (error) {
     console.error("Place Autocomplete Error:", error.message);
@@ -123,7 +126,7 @@ app.get("/api/address-recommend", async (req, res) => {
 
     const swiggyURL = `https://www.swiggy.com/dapi/misc/address-recommend?place_id=${place_id}`;
 
-    const response = await axios.get(swiggyURL, {
+    const response = await client.get(swiggyURL, {
       headers: {
         "User-Agent": "Mozilla/5.0",
         Accept: "application/json",
@@ -133,7 +136,11 @@ app.get("/api/address-recommend", async (req, res) => {
       },
     });
 
-    res.setHeader("Access-Control-Allow-Origin", "*");
+    const origin = req.headers.origin;
+    res.set({
+      "Access-Control-Allow-Origin": origin,
+      "Access-Control-Allow-Methods": "GET",
+    });
     res.status(200).json(response.data);
   } catch (error) {
     console.error("Address Recommend Error:", error.message);
@@ -153,7 +160,7 @@ app.get("/api/address-from-coordinates", async (req, res) => {
 
     const swiggyURL = `https://www.swiggy.com/dapi/misc/address-recommend?latlng=${lat1}%2C${lng1}`;
 
-    const response = await axios.get(swiggyURL, {
+    const response = await client.get(swiggyURL, {
       headers: {
         "User-Agent": "Mozilla/5.0",
         Accept: "application/json",
@@ -163,7 +170,11 @@ app.get("/api/address-from-coordinates", async (req, res) => {
       },
     });
 
-    res.setHeader("Access-Control-Allow-Origin", "*");
+    const origin = req.headers.origin;
+    res.set({
+      "Access-Control-Allow-Origin": origin,
+      "Access-Control-Allow-Methods": "GET",
+    });
     res.status(200).json(response.data);
   } catch (error) {
     console.error("Address from Coordinates Error:", error.message);
@@ -186,7 +197,7 @@ app.get("/api/specific-restaurants", async (req, res) => {
   const mainUrl = `https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=${lat}&lng=${lng}&restaurantId=${id}&catalog_qa=undefined&submitAction=ENTER`;
 
   try {
-    const response = await axios.get(mainUrl, {
+    const response = await client.get(mainUrl, {
       headers: {
         "User-Agent": "Mozilla/5.0",
         Accept: "application/json",
@@ -196,7 +207,11 @@ app.get("/api/specific-restaurants", async (req, res) => {
       },
     });
 
-    res.setHeader("Access-Control-Allow-Origin", "*");
+    const origin = req.headers.origin;
+    res.set({
+      "Access-Control-Allow-Origin": origin,
+      "Access-Control-Allow-Methods": "GET",
+    });
     res.status(200).json(response.data);
   } catch (err) {
     console.error("Restaurant data can't be fetched: ", err.message);
@@ -219,7 +234,7 @@ app.get("/api/dish-search", async (req, res) => {
   const searchUrl = `https://www.swiggy.com/dapi/menu/pl/search?lat=${lat}&lng=${lng}&restaurantId=${restro_Id}&isMenuUx4=true&query=${searchTerm}&submitAction=ENTER`;
 
   try {
-    const response = await axios.get(searchUrl, {
+    const response = await client.get(searchUrl, {
       headers: {
         "User-Agent": "Mozilla/5.0",
         Accept: "application/json",
@@ -229,7 +244,11 @@ app.get("/api/dish-search", async (req, res) => {
       },
     });
     // console.log(response);
-    res.setHeader("Access-Control-Allow-Origin", "*");
+    const origin = req.headers.origin;
+    res.set({
+      "Access-Control-Allow-Origin": origin,
+      "Access-Control-Allow-Methods": "GET",
+    });
     res.status(200).json(response.data);
   } catch (err) {
     console.log("Dish search data can't be fetched; ", err);
