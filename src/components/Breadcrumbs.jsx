f724ef8585d45548d9c63b3575195539b74540a1
Admin <shivendrawk@gmail.com>
2025-06-17 14:51:29 +0530

    commit before pull

--- src/components/Breadcrumbs.jsx
+++ src/components/Breadcrumbs.jsx
@@ -3,24 +3,32 @@ import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
 const Breadcrumbs = () => {
-    const pathHistory = useSelector(selectUserFriendlyPathHistory);
-    const navigate = useNavigate();
+  const pathHistory = useSelector(selectUserFriendlyPathHistory);
+  const navigate = useNavigate();
 
-    return <div className="inline-flex gap-0.5">
-        {
-            pathHistory.map((path, index, arr)=> {
-                const lastIndex = arr.length - 1;
-                const backstage = (index + 1) - arr.length;
-                const delemeter = (index < lastIndex && index > 0) ? "/" : "";
+    const clickHandler = (index, lastIndex) => {
+        if (index === )
+    }
 
-                return (
-                    <button onClick={() => navigate(backstage)} style={{color: index === lastIndex? "#101828" : "#4a5565"}}>
-                        {`${delemeter} ${path}`}
-                    </button>
-                )
-            })
-        }
+  return (
+    <div className="inline-flex gap-0.5 items-center">
+      {pathHistory.map((path, index, arr) => {
+        const lastIndex = arr.length - 1;
+        const backstage = index + 1 - arr.length;
+        const delemeter = index < lastIndex && index > 0 ? "/" : "";
+
+        return (
+          <button
+            onClick={() => navigate(backstage)}
+            style={{ color: index === lastIndex ? "#101828" : "#4a5565" }}
+            className="cursor-pointer"
+          >
+            {`${delemeter} ${path}`}
+          </button>
+        );
+      })}
     </div>
-}
+  );
+};
 
 export default Breadcrumbs;
