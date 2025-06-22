8d2f53bc84dd148f6810d9da8721ad70642acdee
Admin <shivendrawk@gmail.com>
2025-06-22 13:21:08 +0530

    Make menu list responsive

--- src/components/RestaurantSpecific/Menu.jsx
+++ src/components/RestaurantSpecific/Menu.jsx
@@ -17,7 +17,7 @@ const Menu = () => {
       onClick={() => dispatch(toggleMenuModel())}
       className="fixed inset-0 bg-black/30 top-0 left-0 w-[100%] h-[100%] z-40"
     >
-      <div className="fixed bottom-16 left-1/2 transform -translate-x-1/2 bg-primary hide-scrollbar text-gray-300 max-h-80 overflow-auto dark:bg-gray-600 px-3 py-1 rounded-xl opacity-100 z-50">
+      <div className="fixed bottom-16 left-1/2 transform -translate-x-1/2 bg-primary hide-scrollbar text-gray-300 max-h-80 overflow-auto dark:bg-gray-600 px-3 py-1 rounded-xl opacity-100 z-50 max-md:min-w-[70%] max-md:max-w-[80%]">
         {menuItems.length > 0 &&
           menuItems.map((item) => {
             const path = item?.replace(/\s/g, "-");
@@ -28,7 +28,7 @@ const Menu = () => {
                 onClick={() => clickHandler(path)}
                 className="block py-2 w-full font-bold text-white hover:bg-gray-400 p-3 rounded-xl"
               >
-                <p>{item}</p>
+                <p className="">{item}</p>
               </button>
             );
           })}
