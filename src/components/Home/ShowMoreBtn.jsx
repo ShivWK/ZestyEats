73c1626c1ea9524560281b67ebb545cb37c77e17
Admin <shivendrawk@gmail.com>
2025-06-23 13:12:55 +0530

    Make site responsive

--- src/components/Home/ShowMoreBtn.jsx
+++ src/components/Home/ShowMoreBtn.jsx
@@ -2,7 +2,7 @@ const ShowMoreBtn = ({ handleClick }) => {
   return (
     <button
       onClick={ handleClick }
-      className="flex items-center justify-center gap-3 font-bold text-primary w-48 md:w-56 h-16 border-[1px] border-gray-400 px-5 rounded-2xl cursor-pointer active:scale-95 hover:text-white hover:bg-primary transition-all duration-100 ease-in-out"
+      className="flex items-center justify-center gap-3 font-bold text-primary w-44 md:w-56 h-16 border-[1px] border-gray-400 px-5 rounded-2xl cursor-pointer active:scale-95 hover:text-white hover:bg-primary transition-all duration-100 ease-in-out"
     >
       Show More
       <i className="fa-solid fa-caret-down"></i>
