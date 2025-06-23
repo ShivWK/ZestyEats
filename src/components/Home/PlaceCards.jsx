7509a1457d42d7e96c45b37481523a9f279aa028
Admin <shivendrawk@gmail.com>
2025-06-23 13:17:21 +0530

    Make site responsive

--- src/components/Home/PlaceCards.jsx
+++ src/components/Home/PlaceCards.jsx
@@ -1,7 +1,7 @@
 import { NavLink } from "react-router-dom";
 
 const PlaceCards = ({ data }) => {
-    return <NavLink className="flex justify-center items-center text-center h-16 flex-wrap border-[1px] border-gray-400 w-44 md:w-56 px-5 rounded-2xl hover:shadow-2xl hover:scale-[1.01] transition-all duration-100 ease-in">
+    return <NavLink className="flex justify-center items-center text-center flex-wrap border-[1px] border-gray-400 w-44 md:w-56 px-5 py-2 rounded-2xl hover:shadow-2xl hover:scale-[1.01] transition-all duration-100 ease-in">
         <p className="font-medium text-gray-900 ">{data.text}</p>
     </NavLink>
 }
