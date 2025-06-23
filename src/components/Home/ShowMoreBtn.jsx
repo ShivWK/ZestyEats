3d44b05c2dc297e2a6697f4e23d2862784c0dc09
Admin <shivendrawk@gmail.com>
2025-06-23 13:45:11 +0530

    Make site responsive

--- src/components/Home/ShowMoreBtn.jsx
+++ src/components/Home/ShowMoreBtn.jsx
@@ -5,7 +5,7 @@ const ShowMoreBtn = ({ handleClick }) => {
       className="flex items-center justify-center gap-2 md:gap-3 font-bold text-primary w-[48%] md:w-56 py-2 border-[1px] border-gray-400 max-md:text-xl px-3 md:px-5 rounded-2xl cursor-pointer active:scale-95 hover:text-white hover:bg-primary transition-all duration-100 ease-in-out"
     >
       Show More
-      <i className="fa-solid fa-caret-down"></i>
+      <i className="fa-solid fa-caret-down max-md:text-xl"></i>
     </button>
   );
 };
