619d65b61c4a45168f68a832b6db4d09a483c893
Shivendra Dwivedi <shivendrawk@gmail.com>
2025-06-25 22:17:12 +0530

    MAke site responsive

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,6 +1,6 @@
 const Loader = ({ size }) => {
   return size === "large" ? (
-    <div className="w-full h-[30%] md:h-[400px] bg-gray-950 flex justify-center items-center pt-20">
+    <div className="w-full h-[250px] md:h-[400px] bg-gray-950 flex justify-center items-center pt-20">
       <div className="main-large"></div>
     </div>
   ) : (
