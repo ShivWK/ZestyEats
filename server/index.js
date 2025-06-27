058cccecb12a55aa329769611884c8dfacedd2a3
Shivendra Dwivedi <shivendrawk@gmail.com>
2025-06-27 11:29:27 +0530

    Add new origin

--- server/index.js
+++ server/index.js
@@ -11,6 +11,7 @@ const PORT = process.env.PORT || 5000;
 const allowedOrigins = [
   "http://localhost:5173",
   "https://swiggy-clone-beige-gamma.vercel.app",
+  "https://zestyeats-beige-gamma.vercel.app/"
 ];
 
 app.use(
