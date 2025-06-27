b1cd474b626dcce43943975a793220994be0c2a2
Shivendra Dwivedi <shivendrawk@gmail.com>
2025-06-28 02:43:28 +0530

    create mobile bottom menu

--- src/components/Footer/MobileFooterMenu.jsx
+++ src/components/Footer/MobileFooterMenu.jsx
@@ -23,7 +23,7 @@ const MobileFooterMenu = () => {
 
     return (
         <div
-            className="flex md:hidden w-full items-center justify-around text-2xl text-black h-14 shadow-[0_0_14px_1px_rgba(0,0,0,0.5)] position fixed bottom-0 transform transition-transform duration-200 ease-linear bg-white"
+            className="flex md:hidden w-full items-center justify-around text-2xl text-black h-14 shadow-[0_0_20px_1px_rgba(0,0,0,0.3)] position fixed bottom-0 transform transition-transform duration-200 ease-linear bg-white"
             style={{ transform: show ? "translateY(0)" : "translateY(100%)" }}
         >
             <button>
