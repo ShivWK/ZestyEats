f32c5d072c7996fd74b31791f0679b311d3a5793
Shivendra Dwivedi <shivendrawk@gmail.com>
2025-07-04 17:03:32 +0530

    Fix wishlist page and move data from wishlist to cart

--- src/components/MobilePages/Wishlisttems/ItemCard.jsx
+++ src/components/MobilePages/Wishlisttems/ItemCard.jsx
@@ -69,7 +69,7 @@ const ItemCard = ({ item, restro_id }) => {
   console.log("cart", cart);
 
   return (
-    <div className="border-[1px] border-gray-300 p-0.5">
+    <div className="border-t-[1px] border-gray-300 p-0.5">
       <div className="flex justify-between items-center">
         <input
           className="h-4 w-4 ml-1 mt-1"
