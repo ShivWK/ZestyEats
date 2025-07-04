3201dbcbc315d156b9bc67bdf3b7784bbe370c0e
Shivendra Dwivedi <shivendrawk@gmail.com>
2025-07-05 01:01:13 +0530

    Change Delete icon

--- src/components/MobilePages/Wishlisttems/ItemCard.jsx
+++ src/components/MobilePages/Wishlisttems/ItemCard.jsx
@@ -77,12 +77,22 @@ const ItemCard = ({ item, restro_id }) => {
           checked={checked}
           onChange={checkHandler}
         />
-        <button
+        {/* <button
           onClick={deleteHandler}
           className="text-xl text-gray-600 mr-1 mt-0.5"
         >
           <i className="fa-solid fa-trash-can"></i>
-        </button>
+        </button> */}
+        <div className="cursor-pointer flex items-center justify-center rounded-[9999px] p-0.5 mr-0.5 mt-0.5" onClick={deleteHandler} style={{ backgroundColor: "red" }}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="white"
+            className="w-5 h-5 wishlist-icon"
+          >
+            <path d="M20.2426 4.75736C22.5053 7.02472 22.583 10.637 20.4786 12.993L11.9999 21.485L3.52138 12.993C1.41705 10.637 1.49571 7.01901 3.75736 4.75736C6.02157 2.49315 9.64519 2.41687 12.001 4.52853C14.35 2.42 17.98 2.49 20.2426 4.75736ZM5.17157 6.17157C3.68183 7.66131 3.60704 10.0473 4.97993 11.6232L11.9999 18.6543L19.0201 11.6232C20.3935 10.0467 20.319 7.66525 18.827 6.1701C17.3397 4.67979 14.9458 4.60806 13.3743 5.98376L9.17157 10.1869L7.75736 8.77264L10.582 5.946L10.5002 5.87701C8.92545 4.61197 6.62322 4.71993 5.17157 6.17157Z"></path>
+          </svg>
+        </div>
       </div>
       <div className="flex justify-between bg-white">
         <div className="flex flex-col gap-2 p-4">
