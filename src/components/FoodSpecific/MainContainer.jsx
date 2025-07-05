978117cd8797519372896f7ae8cb027129876a1c
Shivendra Dwivedi <shivendrawk@gmail.com>
2025-07-05 16:12:07 +0530

    Fix Animation

--- src/components/FoodSpecific/MainContainer.jsx
+++ src/components/FoodSpecific/MainContainer.jsx
@@ -67,8 +67,6 @@ const MainContainer = ({ data }) => {
       <div className="flex items-center justify-center gap-2 w-fit my-2 animate-dishes">
         <i className="ri-verified-badge-fill text-xl md:text-2xl text-black"></i>
         <i className="ri-bowl-fill text-2xl md:text-3xl text-black"></i>
-        {/* <i className="ri-cake-2-fill text-2xl md:text-3xl text-black"></i>
-        <i className="ri-goblet-fill text-2xl md:text-3xl text-black"></i> */}
         <i className="ri-e-bike-2-fill text-2xl md:text-3xl text-black"></i>
       </div>
     </div>
