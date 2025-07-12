7fc670f44645148c67f35fdf90021179dd841103
Shivendra Dwivedi <shivendrawk@gmail.com>
2025-07-12 14:18:56 +0530

    Add close icon

--- src/components/Cart/Billing.jsx
+++ src/components/Cart/Billing.jsx
@@ -68,6 +68,7 @@ const Billing = () => {
     const containerClickHandler = () => {
         setCouponsOpen(false);
         setOpenInfo(false)
+        console.log("Call")
     }
 
     return <section className="rounded-md md:basis-[39%] bg-white p-2 md:p-5">
@@ -79,7 +80,7 @@ const Billing = () => {
                     {coupon ? <p className="font-bold text-gray-500">{coupon}</p>
                         : <p>Apply Coupon</p>}
                 </div>
-                <i className="ri-arrow-drop-down-fill text-4xl text-black transform transition-transform duration-200 ease-linear" style={{
+                <i className="fa-solid fa-caret-down  text-black transform transition-transform duration-200 ease-linear" style={{
                     transform: couponsOpen && "rotate(-180deg)"
                 }}></i>
             </div>
@@ -99,20 +100,21 @@ const Billing = () => {
                     <span className="text-gray-700 font-semibold">₹{deliveryFee}</span>
                 </div>
                 <div className="flex justify-between py-1 pt-1.5 border-t-[1px] mt-2 border-gray-400 border-dashed">
-                    <p className="relative flex gap-1 items-center">
+                    <div className="relative flex gap-1 items-center">
                         <span className="text-gray-600">GST & Other Charges</span>
                         <i onClick={(e) => {
                             e.stopPropagation()
 
-                            if (window.innerWidth > 768) return;
+                            // if (window.innerWidth > 768) return;
                             setOpenInfo(!openInfo)
+                            console.log("Cll")
                         }}
-                            className="ri-information-2-line cursor-pointer text-[16px]"
+                            className={`${openInfo ? "ri-close-circle-fill" : "ri-information-2-line"} cursor-pointer text-[16px]`}
                             onMouseEnter={() => setOpenInfo(true)}
                             onMouseLeave={() => setOpenInfo(false)}
                         />
 
-                        <div onBlur={(e) => e.stopPropagation()} id="dropdown" className="absolute -top-[520%] left-3/4 h-28 w-40 rounded-md p-2 drop-shadow-[0_0_5px_rgba(0,0,0,0.5)] bg-white" style={{
+                        <div onClick={(e) => e.stopPropagation()} id="dropdown" className="absolute -top-[520%] left-3/4 h-28 w-40 rounded-md p-2 drop-shadow-[0_0_5px_rgba(0,0,0,0.5)] bg-white" style={{
                             display: openInfo ? "block" : "none"
                         }}>
                             <div onClick={(e) => e.stopPropagation()} className="relative h-full">
@@ -120,7 +122,7 @@ const Billing = () => {
                                 <div className="absolute top-[108%] left-3.5 bottom-full h-0 w-0 border-t-8 border-t-white border-l-8 border-r-8 border-r-transparent border-l-transparent"></div>
                             </div>
                         </div>
-                    </p>
+                    </div>
                     <span className="text-gray-700 font-semibold">₹{GSTAndOther}</span>
                 </div>
                 <div className="flex justify-between py-2 border-t mt-2 pt-2">
