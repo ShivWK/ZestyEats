9a67c4134c0f2690676ce3b4c9a551d1b3e09997
Shivendra Dwivedi <shivendrawk@gmail.com>
2025-07-16 16:15:42 +0530

    Fix Foodie thoughts

--- src/components/Home/FoodieThoughts/Cards.jsx
+++ src/components/Home/FoodieThoughts/Cards.jsx
@@ -32,7 +32,7 @@ const Cards = memo(({ data }) => {
   return (
     <Link to={path} onClick={handleClick} className="shrink-0 w-28 lg:w-36">
       <img
-        className="w-full h-[7rem] my-3 lg:my-2 lg:h-44 rounded object-cover"
+        className="w-full h-[9rem] my-3 lg:my-2 lg:h-44 rounded object-cover"
         src={isError ? "/images/fallback.png" : `https://media-assets.swiggy.com/swiggy/image/upload/${data?.imageId}`}
         alt={data?.accessibility?.altText}
         onError={() => setIsError(true)}
