9a55b369ab01887101c723ebd47b4c4a7f0b09b7
Shivendra Dwivedi <shivendrawk@gmail.com>
2025-07-17 12:25:46 +0530

    Fix menu model

--- src/components/RestaurantSpecific/Menu.jsx
+++ src/components/RestaurantSpecific/Menu.jsx
@@ -60,9 +60,9 @@ const Menu = () => {
                     dispatch(setHideMenu(true));
                     e.stopPropagation();
                   }}
-                  className="block py-2 w-full font-bold text-white hover:bg-gray-400 active:bg-gray-400 px-4 rounded-lg"
+                  className="block py-1.5 lg:py-2 w-full font-bold text-white hover:bg-gray-400 active:bg-gray-400 px-4 rounded-lg"
                 >
-                  <p>{textToZestyEats(item.title)}</p>
+                  <p className="leading-5">{textToZestyEats(item.title)}</p>
                 </button>
               );
             })
