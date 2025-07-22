e27779e4e42f08a29467070c47b50d2d0dccb725
Shivendra Dwivedi <shivendrawk@gmail.com>
2025-07-22 21:39:18 +0530

    Fix login model

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -196,7 +196,7 @@ const Login = () => {
 
   return (
     <>
-      <div id="toggle" onClick={() => setOtpOnPhone(!otpOnPhone)} className="relative mt-4 border-2 border-gray-200 rounded-full w-16 h-8 flex items-center justify-between cursor-pointer  bg-gray-200">
+      <div id="toggle" onClick={() => setOtpOnPhone(!otpOnPhone)} className="relative mt-4 lg:mt-2 border-2 border-gray-200 rounded-full w-16 h-8 flex items-center justify-between cursor-pointer  bg-gray-200">
         <Phone size={18} strokeWidth={2} className="text-green-500 ml-1.5" />
         <Mail size={18} strokeWidth={2} className="text-red-500 mr-1.5" />
         <div className={`absolute rounded-full h-full right-0 w-7 transition-all duration-150 ease-linear flex items-center justify-center bg-white`}
@@ -207,6 +207,23 @@ const Login = () => {
           }
         </div>
       </div>
+      {otpOnPhone ? (
+        <p className="text-xs mt-2 font-bold text-gray-600 tracking-wide">
+          OTP will be sent to your{" "}
+          <span className="text-black">
+            Indian mobile number (+91 only).
+          </span>
+        </p>
+      )
+        : (
+          <p className="text-xs mt-2 font-bold text-gray-600 tracking-wide">
+            OTP will be sent to given email.{" "}
+            <span className="text-black">
+              Check spam if missing.
+            </span>
+          </p>
+        )
+      }
       <Form
         btnId="LoginBtn"
         reference={formRef}
