14146d47a8eac197977f9b49068e17e18e24ce1e
Shivendra Dwivedi <shivendrawk@gmail.com>
2025-07-23 17:19:18 +0530

    Edit signup end point

--- server/utils/recaptchaVerification.js
+++ server/utils/recaptchaVerification.js
@@ -1,16 +1,18 @@
 // const fetch = require("node-fetch");
-const querystring = require("querystring");
+// const querystring = require("querystring");
 
 const recaptchaVerification = async (token) => {
     const secret = process.env.RECAPTCHA_SECRET_KEY;
 
+    const params = new URLSearchParams();
+    params.append("secret", secret);
+    params.append('response', token)
+
     try {
         const res = await fetch("https://www.google.com/recaptcha/api/siteverify", {
             method: "POST",
-            body: querystring.stringify({
-                secret,
-                response: token
-            })
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+            body: params
         })
 
         const data = await res.json();
