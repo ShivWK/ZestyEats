2f1b0aced0b55114a6886f8812952b01d9d0594a
Shivendra Dwivedi <shivendrawk@gmail.com>
2025-07-26 13:37:54 +0530

    Fix Badge sizes

--- src/utils/PureVegSvg.jsx
+++ src/utils/PureVegSvg.jsx
@@ -1,4 +1,4 @@
-const PureVeg = () => <span className="flex w-fit items-center gap-1.5 border  border-green-600 px-3 rounded-2xl dark:bg-gray-800/80 bg-gray-100 lg:my-1">
+const PureVeg = () => <span className="flex w-fit items-center gap-1.5 border  border-green-600 px-2 rounded-2xl dark:bg-gray-800/80 bg-gray-100 lg:my-1">
   <i className="fas fa-leaf dark:text-green-400 text-green-600"></i>
   <p className="dark:text-green-400 text-green-600 text-sm font-semibold">Pure Veg</p>
 </span>
