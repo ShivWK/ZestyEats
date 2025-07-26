07a0e49d08ee33c133dc89dc8ccd104d10e70d48
Shivendra Dwivedi <shivendrawk@gmail.com>
2025-07-26 13:43:19 +0530

    Fix Badge sizes

--- src/utils/VegAndNonVegSvg.jsx
+++ src/utils/VegAndNonVegSvg.jsx
@@ -2,7 +2,7 @@ import { Ham } from "lucide-react";
 
 const VegAndNonVeg = ({ classes }) => (
   <span
-    className={`items-center gap-1.5 dark:bg-gray-800/80 bg-gray-100 text-xs font-medium rounded-full border border-gray-300 w-fit text-gray-700 ${classes}`}
+    className={`flex w-fit items-center gap-1.5 border  border-green-600 px-2 rounded-2xl dark:bg-gray-800/80 bg-gray-100 lg:my-1`}
   >
     <i className="fas fa-leaf dark:text-green-400 text-green-600"></i>
     <span className="text-green-700 dark:text-green-400 font-semibold text-sm">Veg</span>
