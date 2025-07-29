ccc69bc9f3673f556c442b11c9634d19eed834ee
Shivendra Dwivedi <shivendrawk@gmail.com>
2025-07-29 15:33:24 +0530

    Change  email OTP logic

--- server/utils/email.js
+++ server/utils/email.js
@@ -3,14 +3,14 @@ const  { Resend } = require("resend");
 const sendMail = async (userEmail, text) => {
     const resend = new Resend(process.env.RESEND_API_KEY);
 
-    const send = resend.send({
+    const send = await resend.emails.send({
         from: "ZestyEats <zestyeats@shivendra.site>",
         to: userEmail,
         subject: "OTP from ZestyEats",
         html: text
     })
 
-    return await send;
+    return send;
 }
 
 module.exports = sendMail;
