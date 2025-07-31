578a7ef09606c6598fc08f287bee26fd77ce57ee
Shivendra Dwivedi <shivendrawk@gmail.com>
2025-07-31 21:19:35 +0530

    Edit Profile section

--- src/components/MobileProfile.jsx
+++ src/components/MobileProfile.jsx
@@ -12,8 +12,8 @@ const MobileProfile = () => {
             <div className="flex items-center justify-between mt-2.5">
                 <div>
                     <h1 className="text-2xl text-white dark:text-gray-100 text-shadow-2xs">Shivendra Dwivedi</h1>
-                    <p className="font-medium text-gray-800 dark:text-gray-200">7897532327</p>
-                    <p className="font-medium text-gray-800 dark:text-gray-200 -mt-1 tracking-wide">shivendra@shivendra.site</p>
+                    <p className="font-medium text-gray-200 dark:text-gray-200">7897532327</p>
+                    <p className="font-medium text-gray-200 dark:text-gray-200 -mt-1 tracking-wide">shivendra@shivendra.site</p>
                 </div>
                 <button className="px-5 py-0.5 rounded-md font-sans font-bold tracking-wide text-white bg-blue-600">
                     Edit
