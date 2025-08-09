df8cc263701156d5ffeecb1cfd55bdd4000e54c7
Shivendra Dwivedi <shivendrawk@gmail.com>
2025-08-09 14:07:17 +0530

    Ceate address form

--- src/components/Profile/Address.jsx
+++ src/components/Profile/Address.jsx
@@ -2,111 +2,129 @@ import { useState, useRef, useEffect } from "react";
 
 const Address = (data) => {
     // console.log(data);
-    const [countries, setCountries] = useState([]);
-    const [selectedCountry, setSelectedCountry] = useState("Select Country")
-    const [openDropDown, setOpenDropDown] = useState(false)
+    const [searchedCountries, setSearchedCountries] = useState([]);
+    const [allCountries, setAllCountries] = useState([]);
+    const [selectedCountry, setSelectedCountry] = useState("");
+    const [openDropDown, setOpenDropDown] = useState(false);
     const [showForm, setShowForm] = useState(false);
     const formRef = useRef(null);
 
     useEffect(() => {
         fetch("https://restcountries.com/v3.1/all?fields=name,cca2,flag")
-            .then(res => res.json())
-            .then(data => setCountries(data))
-            .catch(err => {
+            .then((res) => res.json())
+            .then((data) => setAllCountries(data))
+            .catch((err) => {
                 console.log("Failed to fetch countries data", err);
-            })
-
-        console.log(countries.find(data => data.cca2 === "SH"))
-
-    }, [])
+            });
+    }, []);
 
     const countrySelectHandler = (e) => {
-        setCountries(e.target.value);
+        setSelectedCountry(e.target.value);
+
+        const searchedData = allCountries.filter((data) =>
+            data.name.common.toLowerCase().startsWith(selectedCountry.toLowerCase())
+        );
+        setSearchedCountries(searchedData);
+    };
+
+
+
+    const countryClickHandler = (e) => {
+        e.stopPropagation();
+        const country = e.target.innerText;
+        setSelectedCountry(country);
+
+        setTimeout(() => setOpenDropDown(false), 100)
+    };
+
+    let mapOnData = [];
+
+    if (selectedCountry.length !== 0) {
+        mapOnData = searchedCountries;
+    } else {
+        mapOnData = allCountries;
     }
 
-    return <section className="px-1 mt-4">
-        {!showForm && (<button onClick={() => setShowForm(true)} className="bg-primary dark:bg-darkPrimary px-3 py-1 rounded-md font-medium text-white">
-            Add Address
-        </button>)}
+    useEffect(() => {
+        if (searchedCountries.length !== 0) {
+            setOpenDropDown(true);
+        } else {
+            setOpenDropDown(false);
+        }
+    }, [searchedCountries])
 
-        {showForm && <form
-            ref={formRef}
-            className="p-4 lg:p-5 border-[1px] dark:border-2 border-primary w-[95%] lg:w-[70%] max-lg:mx-auto rounded-xl"
-        >
-            <p className="text-sm dark:text-white text-black inline">Country:</p>
-            {"  "}
-            <div className="relative group border bg-gray-100 dark:placeholder:text-gray-600 dark:bg-gray-300 border-primary rounded py-0.5 px-1 w-[80%] inline-flex items-center gap-1">
-                <input type="text" placeholder="select country" className="inline w-full border-none outline-none" value={selectedCountry} onChange={countrySelectHandler}></input>
-                <i
-                    onClick={() => setOpenDropDown(!openDropDown)}
-                    className="fa-solid fa-caret-down dark:group-hover:text-gray-800 text-gray-900 transition-all duration-300 linear"
-                    style={{
-                        transform: openDropDown ? "rotate(-180deg)" : "",
-                    }}
-                ></i>
+    return (
+        <section onClick={() => setOpenDropDown(false)} className="px-1 mt-4 w-full h-full">
+            {!showForm && (
+                <button
+                    onClick={() => setShowForm(true)}
+                    className="bg-primary dark:bg-darkPrimary px-3 py-1 rounded-md font-medium text-white"
+                >
+                    Add Address
+                </button>
+            )}
 
-                <div onBlur={() => setOpenDropDown(false)} className={`absolute top-[120%] ${openDropDown ? "h-80 p-1" : "h-0"} transition-all duration-150 ease-linear overflow-auto bg-gray-100 dark:bg-gray-300 border border-primary left-0 w-full rounded`}>
-                    {countries.map((country, index) => <p key={index} className="p-0.5 rounded leading-5 hover:bg-blue-600 active:bg-blue-500 active:text-white">{country.name.common}</p>)}
-                </div>
-            </div>
+            {showForm && (
+                <form
+                    onClick={() => setOpenDropDown(false)}
+                    ref={formRef}
+                    className="p-4 lg:p-5 border-[1px] dark:border-2 border-primary w-[95%] lg:w-[70%] max-lg:mx-auto rounded-xl"
+                >
+                    <p className="text-sm dark:text-white text-black">Country</p>
+                    
+                    <div className="relative group border bg-gray-100 dark:placeholder:text-gray-600 dark:bg-gray-300 border-primary rounded py-0.5 px-1 w-full inline-flex items-center gap-1">
+                        <input
+                            type="text"
+                            placeholder="select country"
+                            className="inline w-full border-none outline-none truncate"
+                            value={selectedCountry}
+                            onChange={countrySelectHandler}
+                        ></input>
 
+                        <div
+                            onBlur={() => setOpenDropDown(false)}
+                            className={`absolute top-[110%] ${(openDropDown) ? "max-h-0" : "h-0"} drop-shadow-[0_0_5px_rgba(0,0,0,0.5)] transition-all duration-150 ease-linear overflow-auto bg-gray-100 dark:bg-gray-300 left-0 w-full rounded-b-md`}
+                        >
+                            {mapOnData.map((country, index) => (
+                                <p
+                                    onClick={countryClickHandler}
+                                    key={index}
+                                    className="p-0.5 px-1 rounded leading-5 hover:bg-blue-600 active:bg-blue-500 active:text-white"
+                                >
+                                    {country.name.common}
+                                </p>
+                            ))}
+                        </div>
+                    </div>
 
-            {/* <div className="relative mb-5">
-                <input
-                    type="text"
-                    onKeyDown={handleKeyDown}
-                    value={fieldValues.name}
-                    onChange={fieldValuesHandler}
-                    name="name"
-                    placeholder="Enter your name"
-                    className={`w-full bg-gray-100 dark:placeholder:text-gray-600 dark:bg-gray-200 py-2 lg:py-3 outline-none px-3 lg:px-4 rounded-md border-[1px] dark:border-2 ${errorIn === "name" ? "border-red-500" : "border-transparent"}`}
-                />
-                <p
-                    className={`absolute right-1 text-xs font-medium mt-1 ${fieldValues.name.length >= 100 ? "text-red-500" : "text-gray-600 dark:text-gray-300"
-                        }`}
-                >{`${fieldValues.name.length}/100`}</p>
-            </div>
-            <div className="relative mb-5">
-                <input
-                    type="email"
-                    onKeyDown={handleKeyDown}
-                    value={fieldValues.email}
-                    onChange={fieldValuesHandler}
-                    name="email"
-                    placeholder="Enter your email"
-                    className={`w-full bg-gray-100 dark:placeholder:text-gray-600 dark:bg-gray-200 py-2 lg:py-3 outline-none px-3 lg:px-4 rounded-md border-[1px] dark:border-2 ${errorIn === "email" ? "border-red-500" : "border-transparent"}`}
-                />
-                <p
-                    className={`absolute right-1 text-xs font-medium mt-1 ${fieldValues.email.length >= 254 ? "text-red-500" : "text-gray-600 dark:text-gray-300"
-                        }`}
-                >{`${fieldValues.email.length}/254`}</p>
-            </div>
-            <div className="relative mb-5">
-                <textarea
-                    name="query"
-                    onKeyDown={handleKeyDown}
-                    value={fieldValues.query}
-                    onChange={fieldValuesHandler}
-                    placeholder="Type your message..."
-                    className={`w-full bg-gray-100 dark:placeholder:text-gray-600 dark:bg-gray-200 py-2 lg:py-3 outline-none px-3 lg:px-4 rounded-md min-h-48 lg:min-h-60 border-[1px] dark:border-2 ${errorIn === "query" ? "border-red-500" : "border-transparent"}`}
-                ></textarea>
-                <p
-                    className={`absolute right-1 text-xs font-medium ${fieldValues.query.length >= 1000
-                        ? "text-red-500"
-                        : "text-gray-600 dark:text-gray-300"
-                        }`}
-                >{`${fieldValues.query.length}/1000`}</p>
-            </div>
+                    <div className="mt-1.5">
+                        <p className="text-sm dark:text-white text-black">Full Name</p>
+                        <input type="text" name="name" placeholder="Enter your name" className="p-0.5 px-1 truncate border border-primary rounded w-full outline-none bg-gray-100 dark:placeholder:text-gray-600 dark:bg-gray-300" />
+                    </div>
 
-            <input
-                type="checkbox"
-                name="botcheck"
-                className="hidden"
-                style={{ display: "none" }}
-            ></input> */}
-        </form>}
+                    <div className="mt-1.5">
+                        <p className="text-sm dark:text-white text-black">Phone number</p>
+                        <input type="tel" name="phone" placeholder="Enter your phone number" className="p-0.5 px-1 truncate border border-primary rounded w-full outline-none bg-gray-100 dark:placeholder:text-gray-600 dark:bg-gray-300" />
+                    </div>
 
-    </section>
-}
+                    <div className="mt-1.5">
+                        <p className="text-sm dark:text-white text-black">Flat no./ House no./ Building / Company</p>
+                        <input type="text" name="flatNumber" placeholder="Enter flat, house number, building, or company" className="p-0.5 px-1 truncate border border-primary rounded w-full outline-none bg-gray-100 dark:placeholder:text-gray-600 dark:bg-gray-300" />
+                    </div>
+
+                    <div className="mt-1.5">
+                        <p className="text-sm dark:text-white text-black">Landmark</p>
+                        <input type="text" name="flatNumber" placeholder="Enter your landmark" className="p-0.5 px-1 truncate border border-primary rounded w-full outline-none bg-gray-100 dark:placeholder:text-gray-600 dark:bg-gray-300" />
+                    </div>
+
+                    <div className="mt-1.5">
+                        <p className="text-sm dark:text-white text-black">Pin Code</p>
+                        <input type="number" name="flatNumber" placeholder="Enter your pin code" className="p-0.5 px-1 truncate border border-primary rounded w-full outline-none bg-gray-100 dark:placeholder:text-gray-600 dark:bg-gray-300" />
+                    </div>
+                </form>
+            )}
+        </section>
+    );
+};
 
 export default Address;
