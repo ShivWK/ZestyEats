bf02a142a9257aeae1ac3ec4b787d5b242ef881e
Shivendra Dwivedi <shivendrawk@gmail.com>
2025-08-20 15:38:30 +0530

    Add Shimmer Effect in orders

--- src/components/Search/Ui3Shimmer.jsx
+++ src/components/Search/Ui3Shimmer.jsx
@@ -7,22 +7,22 @@ const Ui3Shimmer = () => {
                 <div className="h-8 w-20 rounded-3xl shimmerBg"></div>
                 <div className="h-8 w-20 rounded-3xl shimmerBg"></div>
             </div>
-            <div className="flex flex-wrap gap-2.5 gap-y-4 w-full justify-between mt-2.5 bg-gray-200 p-1.5 rounded-md">
+            <div className="flex flex-wrap gap-2.5 gap-y-4 w-full justify-between mt-2.5 bg-gray-200  p-1.5 rounded-md">
                 {arr.map((i) => {
                     return (
-                        <div key={i} className="basis-full  md:basis-[49%] border-2 border-gray-300 rounded-md bg-white">
+                        <div key={i} className="basis-full  md:basis-[49%] border-2 border-gray-300 dark:border-gray-400 rounded-md bg-white dark:bg-gray-300">
                             <div className="flex flex-col gap-2 p-4">
                                 <div className="h-4 w-36 shimmerBg rounded"></div>
                                 <div className="h-4 w-20 shimmerBg rounded"></div>
                             </div>
-                            <hr className="text-gray-300 my-1.5" />
-                            <div className="flex justify-between bg-white">
+                            <hr className="text-gray-300 dark:text-gray-400 my-1.5" />
+                            <div className="flex justify-between bg-white dark:bg-gray-300">
                                 <div className="flex flex-col gap-3 p-4">
                                     <div className="h-4 w-36 shimmerBg rounded"></div>
                                     <div className="h-4 w-32 shimmerBg rounded"></div>
                                     <div className="h-4 w-20 shimmerBg rounded"></div>
                                 </div>
-                                <div className="h-36 w-36 shimmerBg rounded m-1.5 bg-white" />
+                                <div className="h-36 w-36 shimmerBg rounded m-1.5 bg-white dark:bg-gray-300" />
                             </div>
                         </div>
                     );
--- src/components/orders/OrderShimmer.jsx
+++ src/components/orders/OrderShimmer.jsx
@@ -6,13 +6,13 @@ const OrderShimmer = () => {
             <div className="flex flex-wrap gap-2.5  w-full justify-between mt-0.5 bg-gray-200 p-1 rounded-md">
                 {arr.map((i) => {
                     return (
-                        <div key={i} className="basis-full md:basis-[49%] pb-2 border-2 border-gray-300 rounded-md bg-white">
+                        <div key={i} className="basis-full md:basis-[49%] pb-2 border-2 border-gray-300 dark:border-gray-400 rounded-md bg-white dark:bg-gray-300">
                             <div className="flex flex-col gap-2.5 p-3 w-full">
                                 <div className="h-4 w-[80%] shimmerBg rounded"></div>
                                 <div className="h-3 w-[70%] shimmerBg rounded"></div>
                             </div>
-                            <hr className="text-gray-300 my-0.5" />
-                            <div className="flex justify-between bg-white">
+                            <hr className="text-gray-300 dark:text-gray-400 my-0.5" />
+                            <div className="flex justify-between bg-white dark:bg-gray-300">
                                 <div className="flex flex-col gap-3 p-3 w-full">
                                     <div className="h-4 w-[90%] shimmerBg rounded"></div>
                                     <div className="h-4 w-[80%] shimmerBg rounded"></div>
